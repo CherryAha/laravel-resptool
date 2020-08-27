b1af50ac09d181a658df36b3bbb2406d50aafc78
shou <1152804113@qq.com>
2020-08-27 14:42:23 +0800

    'v0.1'

---
+++ src/RespToStr.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RespStr
+
+class RespToStr
+{
+    public function toString($condition)
+    {
+        //解析代码
+        $statment = "";
+        foreach ($condition as $id=>$item)
+        {
+            if (isset($item[0]) && isset($item[1]) && isset($item[2]) && isset($item[3]))
+            {
+                $statment_return = "return response()->json(['code'=>".$item[2].",'msg'=>".$item[3].",data=>".$item[4]."]);";
+
+                //判断条件
+                switch ($item[0])
+                {
+                    case 'if':
+                        $statment = $statment.'if('.$item[1].'){'.$statment_return.'}';
+                        break;
+                    case  "if else":
+                        $statment = $statment.'else if('.$item[1].'){'.$statment_return.'}';
+                        break;
+                    case 'else':
+                        $statment = $statment.'else{'.$statment_return.'}';
+                        break;
+                }
+            }
+        }
+        return $statment;
+    }
+}
